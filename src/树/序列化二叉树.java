de1c68b62cc810ad176bb6d8b97731520428ac4a
zpc <924824195@qq.com>
2019-07-23 21:45:21 +0800

    fix 序列化二叉树

--- src/树/序列化二叉树.java
+++ src/树/序列化二叉树.java
@@ -1,5 +1,9 @@
 package 树;
 
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.LinkedList;
+
 public class 序列化二叉树 {
     //请实现两个函数，分别用来序列化和反序列化二叉树。
     private String deserializeStr;
@@ -29,6 +33,23 @@ public class 序列化二叉树 {
         t.right = Deserialize();
         return t;
     }
+    // Decodes your encoded data to tree.
+    public static TreeNode deserialize(String data) {
+        Deque<String> nodes = new LinkedList<>();
+        nodes.addAll(Arrays.asList(data.split(" ")));
+        return buildTree(nodes);
+    }
+
+    private static TreeNode buildTree(Deque<String> nodes) {
+        String val = nodes.remove();
+        if (val.equals("#")) return null;
+        else {
+            TreeNode node = new TreeNode(Integer.valueOf(val));
+            node.left = buildTree(nodes);
+            node.right = buildTree(nodes);
+            return node;
+        }
+    }
 
     public static void main(String[] args) {
         TreeNode root=new TreeNode(1);
@@ -40,6 +61,6 @@ public class 序列化二叉树 {
         String k=t.Serialize(root);
         System.out.println(k);
         System.out.println(t.Deserialize(k));
-
+        System.out.println(deserialize(k));
     }
 }
