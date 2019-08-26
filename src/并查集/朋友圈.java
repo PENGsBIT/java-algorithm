c4ebe21761264463022a5a443fd40c28a923eda8
pengc <924824195@qq.com>
2019-08-26 07:18:12 +0200

    fix

--- src/并查集/朋友圈.java
+++ src/并查集/朋友圈.java
@@ -25,6 +25,7 @@ package 并查集;
 //
 
 public class 朋友圈 {
+    //并查集
     public static int findCircleNum(int[][] M) {
         int m = M.length, cnt = 0;
         int[] root = new int[m];
@@ -51,5 +52,26 @@ public class 朋友圈 {
             {0,0,1},
         };
         System.out.println(findCircleNum(M));
+    }
+
+    //dfs
+    public void dfs(int[][] M, int[] visited, int i) {
+        for (int j = 0; j < M.length; j++) {
+            if (M[i][j] == 1 && visited[j] == 0) {
+                visited[j] = 1;
+                dfs(M, visited, j);
+            }
+        }
+    }
+    public int dfsFindCircleNum(int[][] M) {
+        int[] visited = new int[M.length];
+        int count = 0;
+        for (int i = 0; i < M.length; i++) {
+            if (visited[i] == 0) {
+                dfs(M, visited, i);
+                count++;
+            }
+        }
+        return count;
     }
 }
