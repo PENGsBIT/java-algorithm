a4378df86e6d0fd03653c68a185b9d3790d5d7ef
zhoupengcheng03 <zhoupengcheng03@corp.netease.com>
2022-02-10 15:03:20 +0800

    单例

--- src/设计模式/单例模式/饿汉.java
+++ src/设计模式/单例模式/饿汉.java
@@ -4,6 +4,8 @@ package 设计模式.单例模式;/**
  * @create: 2019-07-19 22:49
  **/
 
+import java.lang.reflect.Constructor;
+
 /**
  * @Author: zpc
  * @Description: 单例
@@ -12,7 +14,20 @@ package 设计模式.单例模式;/**
 
 
 public class 饿汉 {
+    public static void main(String[] args) throws Exception {
+         eHanSingleton s = eHanSingleton.getInstance();
 
+        // 拿到所有的构造函数，包括非public的
+        Constructor<eHanSingleton> constructor = eHanSingleton.class.getDeclaredConstructor();
+        constructor.setAccessible(true);
+        // 使用空构造函数new一个实例。即使它是private的~~~
+        eHanSingleton sReflection = constructor.newInstance();
+
+        System.out.println(s); //com.fsx.bean.Singleton@1f32e575
+        System.out.println(sReflection); //com.fsx.bean.Singleton@279f2327
+        System.out.println(s == sReflection); // false
+        //单例创建出了一个新的实例对象。所以这种方式也还是存在不安全因素的。
+    }
 }
 class eHanSingleton {
      //当类第一次被加载到内存它就实例化了，所以这种实例的创建方式是线程安全的。
